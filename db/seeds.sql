56562ae70b792b4078ee66f91d2888f46b4b9b03
dalex <delphindidier0@gmail.com>
2020-04-28 03:04:49 -0400

    file structure update

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, eaten) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (burger_name, eaten) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, eaten) VALUES ('Quarter Pounder', false);
+INSERT INTO burgers (burger_name, eaten) VALUES ('Baconator', false);
